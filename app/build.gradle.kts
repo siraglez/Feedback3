83afff8c852fbc37ebd387eaa36ae58138e7a7c7
Sira <sgonzmad@myuax.com>
2024-10-27 17:37:45 +0100

    imports y dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.material)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.runtime.saved.instance.state)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
